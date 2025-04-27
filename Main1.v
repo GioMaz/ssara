0cfc27fd179357f6933ffcfcea30a22a1f50252f
GioMaz <giovmaz2003@gmail.com>
2025-04-27 15:57:26 +0200

    extend the dominance relation to other instruction types

--- Main1.v
+++ Main1.v
@@ -191,12 +191,20 @@ Fixpoint eq_list_inst (i1 i2 : list inst) : bool :=
 .
 
 Inductive block : Type :=
-  | Block (ps : list phi) (is : list inst) (js : jinst)
+  | Block (ps : list phi) (is : list inst) (j : jinst)
 
 with jinst : Type :=
   | Jnz (r : reg) (b1 : block) (b2 : block)
   | Jmp (b : block)
   | Halt
+.
+
+Definition jinst_args (j : jinst) : option reg :=
+  match j with
+  | Jnz r _ _ => Some r
+  | Jmp _ => None
+  | Halt => None
+  end
 .
 
 (*
@@ -215,6 +223,10 @@ Definition phis (b : block) : list phi :=
 
 Definition insts (b : block) : list inst :=
   let (_, is, _) := b in is
+.
+
+Definition jinsts (b : block) : jinst :=
+  let (_, _, j) := b in j
 .
 
 Fixpoint eq_jinst (j1 j2 : jinst) : bool :=
@@ -373,8 +385,9 @@ Definition single_assignment_program_phi (pr : program) : Prop :=
 
 Definition single_assignment_program_phi_inst (pr : program) : Prop :=
   forall (b b' : block) (p : phi) (i : inst),
-    (In b pr) /\ (In p (phis b)) /\ (In b' pr) /\ (In i (insts b'))
-    /\ ~ exists (r : reg), ((phi_reg p) = r) /\ ((inst_reg i) = Some r)
+    (In b pr) /\ (In p (phis b)) /\ (In b' pr) /\ (In i (insts b')) -> (
+      ~ exists (r : reg), ((phi_reg p) = r) /\ ((inst_reg i) = Some r)
+    )
 .
 
 Definition single_assignment_program (p : program) : Prop :=
@@ -387,17 +400,26 @@ Definition single_assignment_program (p : program) : Prop :=
   If we know that no virtual register is assigned twice, we can say that:
   for every block, for every phi instruction, for every predecessor, there
   exist a variable defined in that predecessor that is contained in the
-  argument of the phi instruction.
+  argument of the phi instruction and that predecessor doesn't define any other
+  variable used which is an argument for that phi.
   This can be done since we know that two variables with the same name do not
-  exist.
+  exist (i.e. `single_assignment_program` is valid).
 *)
+Definition defines (b : block) (r : reg) : Prop :=
+  (exists (p : phi), (In p (phis b)) /\ (phi_reg p = r)) \/
+  (exists (i : inst), (In i (insts b)) /\ (inst_reg i = Some r))
+.
+
 Definition well_formed_phis (p : program) : Prop :=
   forall (b : block), (In b p) -> (
     forall (ph : phi), (In ph (phis b)) -> (
       forall (pr : block), (predecessor pr b p) -> (
-        exists (r : reg),
-          (In r (phi_args ph))
-          /\ (exists (r' : reg), (In r (phi_args ph)) -> r = r')
+        exists (r : reg), (
+          In r (phi_args ph) /\ defines pr r /\
+          ~ exists (r' : reg), (
+            In r' (phi_args ph) /\ defines pr r' /\ r <> r'
+          )
+        )
       )
     )
   )
@@ -411,10 +433,7 @@ Definition well_formed_phis (p : program) : Prop :=
   instructions that share the same virtual register in our representation we
   only need to check recursively on the predecessors of the basic block where i
   is situated for the existance of i'.
-  TODO: handle dominance relationship between all types of instructions (phis
-  and definitions)
 *)
-
 Fixpoint is_path_of (path : list block) (p : program) : Prop :=
   match path with
   | nil => True
@@ -448,7 +467,7 @@ Definition dominates_block (b b' : block) (p : program) : Prop :=
   )
 .
 
-Definition dominates_inst (i i' : inst) (p : program) : Prop :=
+Definition dominates_inst_inst (i i' : inst) (p : program) : Prop :=
   exists (b : block), (In b p) /\ exists i1 i2, (
     (i1 < i2)
     /\ (nth_error (insts b) i1 = Some i)
@@ -462,16 +481,75 @@ Definition dominates_inst (i i' : inst) (p : program) : Prop :=
   )
 .
 
+Definition dominates_phi_inst (ph : phi) (i : inst) (p : program) : Prop :=
+  exists (b : block), (
+    (In b p) /\ (In ph (phis b)) /\ (In i (insts b))
+  )
+  \/
+  exists (b b' : block), (
+    (dominates_block b b' p)
+    /\ (In ph (phis b))
+    /\ (In i (insts b'))
+  )
+.
+
+(* UNUSED FOR NOW *)
+(*
+Definition dominates_inst_phi (i : inst) (ph : phi) (p : program) : Prop :=
+  exists (b b' : block), (
+    (dominates_block b b' p)
+    /\ (In i (insts b))
+    /\ (In ph (phis b'))
+  )
+.
+
+Definition dominates_phi (ph ph' : phi) (p : program) : Prop :=
+  exists (b b' : block), (
+    (dominates_block b b' p)
+    /\ (In ph (phis b))
+    /\ (In ph' (phis b'))
+  )
+.
+*)
+
+Definition dominates_phi_jinst (ph : phi) (j : jinst) (p : program) : Prop :=
+  exists (b : block), (
+    (In b p) /\ (In ph (phis b)) /\ j = (jinsts b)
+  )
+  \/
+  exists (b b' : block), (
+    (dominates_block b b' p)
+    /\ (In ph (phis b))
+    /\ j = (jinsts b')
+  )
+.
+
+Definition dominates_inst_jinst (i : inst) (j : jinst) (p : program) : Prop :=
+  exists (b : block), (
+    (In b p) /\ (In i (insts b)) /\ j = (jinsts b)
+  )
+  \/
+  exists (b b' : block), (
+    (dominates_block b b' p)
+    /\ (In i (insts b))
+    /\ j = (jinsts b')
+  )
+.
+
 Definition assignment_dominates_usage (p : program) : Prop :=
   forall (b : block), (In b p) -> (
     forall (i : inst), (In i (insts b)) -> (
       forall (r : reg), (In r (inst_args i)) -> (
-        exists (i' : inst), (inst_reg i') = Some r /\ dominates_inst i' i p
+        (exists (ph : phi), (phi_reg ph) = r /\ dominates_phi_inst ph i p) \/
+        (exists (i' : inst), (inst_reg i') = Some r /\ dominates_inst_inst i' i p)
       )
     )
+    /\
+    match b with
+    | Block _ _ j => forall (r : reg), jinst_args j = Some r -> (
+        (exists (ph : phi), (phi_reg ph) = r /\ dominates_phi_jinst ph j p) \/
+        (exists (i : inst), (inst_reg i) = Some r /\ dominates_inst_jinst i j p)
+      )
+    end
   )
 .
-
-
-
-
