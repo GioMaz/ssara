4fe42bb52ebdc0dfaa4f72eff86018169d9e3c83
GioMaz <giovmaz2003@gmail.com>
2025-05-14 04:01:58 +0200

    make assumption about phi instructions in order to fix liveness analysis

--- core/LivenessAnalysis.v
+++ core/LivenessAnalysis.v
@@ -73,7 +73,6 @@ Definition option_to_list {A : Type} (x : option A) : list A :=
 
 Fixpoint analyze_As
   {A : Type}
-  (get_args : A -> list reg)
   (get_use : A -> list reg)
   (get_def : A -> list reg)
   (is : list A)
@@ -84,9 +83,8 @@ Fixpoint analyze_As
   | x :: xs =>
     let use := get_use x in
     let def := get_def x in
-    let args := get_args x in
-    let (is', live_out) := analyze_As get_args get_use get_def xs final_live_out in
-    let live_in := args U (regs_minus live_out def) in (
+    let (is', live_out) := analyze_As get_use get_def xs final_live_out in
+    let live_in := use U (regs_minus live_out def) in (
       (Metainst use def live_out live_in) :: is',
       live_in
     )
@@ -97,34 +95,37 @@ Fixpoint analyze_As
   Liveness analysis of a phi instruction
   - use[i]      := {}
   - def[i]      := reg[i]
-  - live_out[i] := U with j ∈ succ[i] of (live_in[j] U args[j])
-  - live_in[i]  := use[i] U (live_out[i] - def[i])
+  - live_out[i] := U with j ∈ succ[i] of live_in[j]
+  - live_in[i]  := live_out[i] - def[i]
 *)
-Definition analyze_phis (ps : list phi) (final_live_out : list reg) : list metainst * list reg :=
+(* Definition analyze_phis_old (ps : list phi) (final_live_out : list reg) : list metainst * list reg :=
   analyze_As phi_args (fun _ => nil) (fun x => option_to_list (phi_reg x)) ps final_live_out
+. *)
+
+Definition analyze_phis (ps : list phi) (final_live_out : list reg) : list metainst * list reg :=
+  analyze_As (fun _ => nil) (fun x => option_to_list (phi_reg x)) ps final_live_out
 .
 
 (*
   Liveness analysis of a normal instruction
   - use[i]      := args[i]
   - def[i]      := reg[i]
-  - live_out[i] := U live_in[j] with j ∈ succ[i]
+  - live_out[i] := U with j ∈ succ[i] of live_in[j]
   - live_in[i]  := use[i] U (live_out[i] - def[i])
 *)
 Definition analyze_insts (is : list inst) (final_live_out : list reg) : list metainst * list reg :=
-  analyze_As inst_args inst_args (fun x => option_to_list (inst_reg x)) is final_live_out
+  analyze_As inst_args (fun x => option_to_list (inst_reg x)) is final_live_out
 .
 
 (*
   Liveness analysis of a jump instruction
   - use[i]      := args[i]
   - def[i]      := {}
-  - live_out[i] := U live_in[j] with j ∈ succ[i]
+  - live_out[i] := U with j ∈ succ[i] live_in[j]
   - live_in[i]  := use[i] U live_out[i]
 *)
 Definition analyze_jinst (j : jinst) (final_live_out : list reg) : list metainst * list reg :=
-  let get_args_use (j : jinst) : list reg := option_to_list (jinst_args j) in
-  analyze_As get_args_use get_args_use (fun _ => nil) (j :: nil) final_live_out
+  analyze_As (fun x => option_to_list (jinst_args j)) (fun _ => nil) (j :: nil) final_live_out
 .
 
 Module Example1.
@@ -151,10 +152,10 @@ End Example2.
 Definition analyze_block (b : block) (final_live_out : list reg) : list metainst * list reg :=
   match b with
   | Block ps is j =>
-    let (m_3, final_live_out_3) := analyze_jinst j final_live_out in
-    let (m_2, final_live_out_2) := analyze_insts is final_live_out_3 in
-    let (m_1, final_live_out_1) := (analyze_phis ps final_live_out_2) in
-    (m_1 ++ m_2 ++ m_3, final_live_out_1)
+    let (m_3, live_in_3) := analyze_jinst j final_live_out in
+    let (m_2, live_in_2) := analyze_insts is live_in_3 in
+    let (m_1, live_in_1) := (analyze_phis ps live_in_2) in
+    (m_1 ++ m_2 ++ m_3, live_in_1)
   end
 .
 
@@ -186,6 +187,24 @@ Module Example4.
   Compute analyze_block example_block nil.
 End Example4.
 
+Fixpoint defines_As {A : Type} (get_reg : A -> option reg) (is : list A) (r : reg) : bool :=
+  match is with
+  | nil => false
+  | x :: xs =>
+    match get_reg x with
+    | Some r' => (r =? r') || defines_As get_reg xs r
+    | None => defines_As get_reg xs r
+    end
+  end
+.
+
+Definition defines (b : block) (r : reg) : bool :=
+  match b with
+  | Block ps is j =>
+    defines_As phi_reg ps r || defines_As inst_reg is r
+  end
+.
+
 Axiom get_fuel : program -> nat.
 
 (* Fixpoint analyze_program (p : program) (fuel : nat) : metablock * list reg :=
@@ -213,21 +232,40 @@ Axiom get_fuel : program -> nat.
 . *)
 
 (*
+  Given list of phis and block return the registers that are both in the block
+  and used as arguemnts of the phis.
+*)
+Definition analyze_block_phis (b : block) (ps : list phi) : list reg :=
+  let args := flat_map phi_args ps in
+  filter (defines b) args
+.
+
+(*
   This is a better version of the previous commented function analyze_program
   that gets the successors of a block using the function `successors` in order
-  to handle possible future branch instructions in a general way
+  to handle possible future branch instructions in a general way.
+
+  The function takes in a program and the maximum depth and returns the
+  metaprogram with the live_in set of the first block which should be the empty
+  set for a well formed SSA program.
+
+  With this function we use the assumption that every phi operation uses only
+  registers that are defined in the immediate predecessor blocks.
 *)
-Fixpoint analyze_program (p : program) (fuel : nat) : metablock * list reg :=
+Fixpoint analyze_program_aux (p : program) (fuel : nat) : metaprogram * list reg :=
   match fuel with
   | O =>
     let (mis_i, live_in_i) := analyze_block p nil in
     (Metablock mis_i nil, live_in_i)
 
   | S fuel' =>
-    (* Analyze successors *)
-    let results := map (fun p => analyze_program p fuel') (successors p) in
+    (* Get successors *)
+    let bs := successors p in
 
-    (* Create mbs, list of successors and live_out union of live_in of successors*)
+    (* Analyze successors *)
+    let results := map (fun p => analyze_program_aux p fuel') bs in
+
+    (* Create mbs, list of successors and live_out, union of live_in of successors *)
     let (mbs, live_out) :=
       fold_left
         (fun '(mbs, live_in) '(mb, live_out) =>
@@ -235,13 +273,59 @@ Fixpoint analyze_program (p : program) (fuel : nat) : metablock * list reg :=
         results
         (nil, nil) in
 
+    (*
+      live_out[i] := U with j ∈ succ[i] of (
+        live_in[j] U { r | r is an argument of a phi instruction of j}
+      )
+
+      +-----------+
+      | IMPORTANT |
+      +-----------+
+      We make the assumption that the phi instructions arguments can only be
+      defined in immediate predecessors this makes our representation of an SSA
+      program incomplete.
+    *)
+    let ps := flat_map phis bs in
+    let defined := analyze_block_phis p ps in
+    let live_out' := live_out U defined in
+
     (* Analyze current block *)
-    let (mis, live_in) := analyze_block p live_out in
+    let (mis, live_in) := analyze_block p live_out' in
 
     (* Create current metablock *)
     (Metablock mis mbs, live_in)
   end
 .
+
+Definition analyze_program (p : program) (fuel : nat) : metaprogram :=
+  let (mp, _) := analyze_program_aux p fuel in mp
+.
+
+Definition filter_defined (regs : list reg) (defined : list reg) : list reg :=
+  filter (fun r => contains r defined) regs
+.
+
+Fixpoint postprocess_mis (mis : list metainst) (defined : list reg) : list metainst * list reg :=
+  match mis with
+  | nil => (nil, defined)
+  | x :: xs =>
+    match x with
+    | Metainst use def live_in live_out =>
+      let curr := Metainst use def (filter_defined live_in defined) (filter_defined live_out defined) in
+      let (next, defined') := postprocess_mis xs (def U defined) in
+      (curr :: next, defined')
+    end
+  end
+.
+
+(* Fixpoint postprocess_mp_aux (mp : metaprogram) (defined : list reg) : metaprogram :=
+  match mp with
+  | Metablock mis mbs =>
+    let (mis', defined') := postprocess_mis mis defined in
+    fold_left
+      (fun '(fmis, fdefined) mp => ) mis' :: (postprocess_mp_aux mbs defined')
+  end
+. *)
 
 Module Example5.
   Definition example_block_2 : block :=
@@ -299,7 +383,7 @@ End Example6.
 (* Interference graph stored using adjacence lists *)
 Definition ig : Type := list (reg * list reg).
 
-Fixpoint ig_insert_edge_aux (r : reg) (r' : reg) (g : ig) : ig :=
+(* Fixpoint ig_insert_edge_aux (r : reg) (r' : reg) (g : ig) : ig :=
   match g with
   | nil => [(r, [r'])]
   | (x, xs) as L :: ys =>
@@ -308,9 +392,18 @@ Fixpoint ig_insert_edge_aux (r : reg) (r' : reg) (g : ig) : ig :=
     else
     L :: ig_insert_edge_aux r r' ys
   end
-.
+. *)
 
 Definition ig_insert_edge (r : reg) (r' : reg) (g : ig) : ig :=
+  let fix ig_insert_edge_aux (r : reg) (r' : reg) (g : ig) : ig :=
+    match g with
+    | nil => [(r, [r'])]
+    | (x, xs) as L :: ys =>
+      if r =? x then
+        (x, regs_insert r' xs) :: ys
+      else
+      L :: ig_insert_edge_aux r r' ys
+    end in
   let g' := ig_insert_edge_aux r r' g in
   ig_insert_edge_aux r' r g'
 .
@@ -368,9 +461,9 @@ Definition get_ig_mp (mp : metaprogram): ig :=
   +-----------+
         |
         |     +-----------+  TODO: handle this specific case where the live_out
-        |     |           |  of the first block is r5 and r6, since they are
-  +-----------------+     |  needed for in phi instructions of the second block
-  | r3 <- Φ(r1, r5) |     |
+        |     |           |  of the first block is {r2, r6, r1, r5}, when
+  +-----------------+     |  instead it should be {r2, r1}. This is caused by
+  | r3 <- Φ(r1, r5) |     |  live_in of the second block being {r2, r6, r1, r5}
   | r4 <- Φ(r2, r6) |     |
   +-----------------+     |
          |                |
@@ -409,10 +502,38 @@ Module Example7.
     )
   .
 
-  Compute analyze_program example_block_1 3.
-  Compute let (mp, _) := analyze_program example_block_1 3 in get_ig_mp mp.
+  Definition fuel : nat := 3.
+
+  Compute analyze_program example_block_1 fuel.
+  Compute get_ig_mp (analyze_program example_block_1 fuel).
 
 End Example7.
+
+Module Example8.
+  Definition example_block_2 : block :=
+    Block [
+    ] [
+      store (Ptr 0) r(0)
+    ] (
+      Halt
+    )
+  .
+
+  Definition example_block_1 : block :=
+    Block [
+    ] [
+      r(0) <- (Imm 34)
+    ] (
+      Jmp example_block_2
+    )
+  .
+
+  Definition fuel : nat := 10.
+
+  Compute analyze_program_aux example_block_1 fuel.
+  Compute get_ig_mp (analyze_program example_block_1 fuel).
+
+End Example8.
 
 (*
 TODO:
--- core/SSA.v
+++ core/SSA.v
@@ -5,11 +5,16 @@ From Stdlib Require Import Lists.List.
   1st property of an SSA program, every instruction is assigned exactly once
 *)
 
-(* Definition single_assignment_program_of {A : Type} (get_sec : block -> list A) (get_reg : A -> option reg) (p : program) : Prop :=
-  forall (b b' : block) (i i' : A),
-    (In b p) /\ (In i (get_sec b)) /\ (In b' p) /\ (In i' (get_sec b')) -> (
+Inductive in_program : block -> program -> Prop :=
+  | IsProgram : forall b, in_program b b
+  | IsSuccessor : forall b b' p, (In b' (successors b)) -> in_program b p -> in_program b' p
+.
+
+Definition single_assignment_program_of {A : Type} (get_sec : block -> list A) (get_reg : A -> option reg) (p : program) : Prop :=
+  forall (b b' : block) (p : program) (i i' : A),
+    in_program b p /\ In i (get_sec b) /\ in_program b' p /\ In i' (get_sec b') -> (
       (get_reg i = get_reg i') -> (
-        (b = b')
+        b = b'
         /\ exists j, (
           nth_error (get_sec b) j = Some i /\ nth_error (get_sec b') j = Some i'
         )
@@ -19,8 +24,8 @@ From Stdlib Require Import Lists.List.
 
 Definition single_assignment_program_phi_inst (pr : program) : Prop :=
   forall (b b' : block) (p : phi) (i : inst),
-    (In b pr) /\ (In p (phis b)) /\ (In b' pr) /\ (In i (insts b')) -> (
-      (phi_reg p) <> (inst_reg i)
+    in_program b pr /\ In p (phis b) /\ in_program b' pr /\ In i (insts b') -> (
+      phi_reg p <> inst_reg i
     )
 .
 
@@ -28,7 +33,7 @@ Definition single_assignment_program (p : program) : Prop :=
   single_assignment_program_of insts inst_reg p /\
   single_assignment_program_of phis phi_reg p /\
   single_assignment_program_phi_inst p
-. *)
+.
 
 (*
   For every block, for every phi instruction, for every predecessor, there
@@ -39,14 +44,20 @@ Definition single_assignment_program (p : program) : Prop :=
   exist (i.e. `single_assignment_program` is valid).
 *)
 Definition defines (b : block) (r : reg) : Prop :=
-  (exists (p : phi), (In p (phis b)) /\ (phi_reg p = Some r)) \/
-  (exists (i : inst), (In i (insts b)) /\ (inst_reg i = Some r))
+  (exists (p : phi), In p (phis b) /\ phi_reg p = Some r) \/
+  (exists (i : inst), In i (insts b) /\ inst_reg i = Some r)
 .
 
-(* Definition well_formed_phis (p : program) : Prop :=
-  forall (b : block), (In b p) -> (
-    forall (ph : phi), (In ph (phis b)) -> (
-      forall (pr : block), (predecessor pr b p) -> (
+(*
+  We make the assumption that a program in SSA form only allows phi
+  instructions that have arguments that are registers defined in the immediate
+  predecessors of the current block and there is an argument for each immediate
+  predecessor, which is what we enforce with this predicate.
+*)
+Definition well_formed_phis (p : program) : Prop :=
+  forall (b : block), in_program b p -> (
+    forall (ph : phi), In ph (phis b) -> (
+      forall (pr : block), predecessor pr b p -> (
         exists (r : reg), (
           In r (phi_args ph) /\ defines pr r /\
           ~ exists (r' : reg), (
@@ -56,7 +67,7 @@ Definition defines (b : block) (r : reg) : Prop :=
       )
     )
   )
-. *)
+.
 
 (*
   2nd property of an SSA program is strictness, that means that an instruction
@@ -67,40 +78,40 @@ Definition defines (b : block) (r : reg) : Prop :=
   register in our representation we only need to check recursively on the
   predecessors of the basic block where i' is situated for the existance of i.
 *)
-(* Fixpoint is_path_of (path : list block) (p : program) : Prop :=
+Fixpoint is_path_of (path : list block) (p : program) : Prop :=
   match path with
   | nil => True
   | x :: nil =>
-    (In x p)
+    in_program x p
   | x :: (y :: ys) as L =>
-    (In x p)
-    /\ (In y (successors x))
-    /\ (is_path_of L p)
+    in_program  x p
+    /\ In y (successors x)
+    /\ is_path_of L p
   end
 .
 
-Definition is_path_from_start (path : list block) (p : program) : Prop :=
+(* Definition is_path_from_start (path : list block) (p : program) : Prop :=
   match path, p with
-  | b :: _, b' :: _ => b = b'
+  | b :: _, p => b = b'
   | _, _ => False
   end
-.
+. *)
 
 Fixpoint comes_before_in (b b' : block) (path : list block) : Prop :=
   match path with
   | nil => False
-  | x :: nil => (x = b) /\ (b = b')
+  | x :: nil => x = b /\ b = b'
   | x :: xs =>
-    ((x = b) /\ (b = b'))
-    \/ ((x = b) /\ (In b' xs))
-    \/ (comes_before_in b b' xs)
+    (x = b /\ b = b')
+    \/ (x = b /\ In b' xs)
+    \/ comes_before_in b b' xs
   end
 .
 
 Definition dominates_block_block (b b' : block) (p : program) : Prop :=
   forall (path : list block), (
-    is_path_of path p /\ is_path_from_start path p /\ hd_error path = Some b /\ last path b = b' -> (
-      forall (x : block), (In x path) -> (
+    is_path_of path p /\ b = p /\ hd_error path = Some b /\ last path b = b' -> (
+      forall (x : block), In x path -> (
         comes_before_in b b' path
       )
     )
@@ -108,22 +119,22 @@ Definition dominates_block_block (b b' : block) (p : program) : Prop :=
 .
 
 Definition dominates_A_A {A : Type} (i : A) (i' : A) (get_sec : block -> list A) (p : program) : Prop :=
-  exists (b : block), (In b p) /\ exists i1 i2, (
-    (i1 < i2)
-    /\ (nth_error (get_sec b) i1 = Some i)
-    /\ (nth_error (get_sec b) i2 = Some i')
+  exists (b : block), (in_program b p) /\ exists i1 i2, (
+    i1 < i2
+    /\ nth_error (get_sec b) i1 = Some i
+    /\ nth_error (get_sec b) i2 = Some i'
   )
   \/
   exists (b b' : block), (
-    (dominates_block_block b b' p)
-    /\ (In i (get_sec b))
-    /\ (In i' (get_sec b'))
+    dominates_block_block b b' p
+    /\ In i (get_sec b)
+    /\ In i' (get_sec b')
   )
 .
 
 Definition dominates_A_B {A : Type} {B : Type} (i : A) (i' : B) (get_A_sec : block -> list A) (get_B_sec : block -> list B) (p : program) : Prop :=
   exists (b : block), (
-    In b p /\ In i (get_A_sec b) /\ In i' (get_B_sec b)
+    in_program b p /\ In i (get_A_sec b) /\ In i' (get_B_sec b)
   )
   \/
   exists (b b' : block), (
@@ -135,21 +146,21 @@ Definition dominates_A_B {A : Type} {B : Type} (i : A) (i' : B) (get_A_sec : blo
 
 Definition dominates_A_jinst {A : Type} (i : A) (j : jinst) (get_A_sec : block -> list A) (p : program) : Prop :=
   exists (b : block), (
-    In b p /\ In i (get_A_sec b) /\ j = (jinsts b)
+    in_program b p /\ In i (get_A_sec b) /\ j = (jinsts b)
   )
   \/
   exists (b b' : block), (
-    (dominates_block_block b b' p)
-    /\ (In i (get_A_sec b))
+    dominates_block_block b b' p
+    /\ In i (get_A_sec b)
     /\ j = (jinsts b')
   )
 .
 
 (* Strictness: every variable usage is dominated by its assignment *)
 Definition strict (p : program) : Prop :=
-  forall (b : block), (In b p) -> (
-    forall (i' : inst), (In i' (insts b)) -> (
-      forall (r : reg), (In r (inst_args i')) -> (
+  forall (b : block), in_program b p -> (
+    forall (i' : inst), In i' (insts b) -> (
+      forall (r : reg), In r (inst_args i') -> (
         (exists (ph : phi), (phi_reg ph) = Some r /\ dominates_A_B ph i' phis insts p) \/
         (exists (i : inst), (inst_reg i) = Some r /\ dominates_A_A i i' insts p)
       )
@@ -162,4 +173,4 @@ Definition strict (p : program) : Prop :=
       )
     end
   )
-. *)
+.
